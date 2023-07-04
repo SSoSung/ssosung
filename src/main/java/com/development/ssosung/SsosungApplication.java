d7b08d346b2a6dfe15c74a21018767516672d506
Hyun Sung Kim <hskim@nexa-ds.com>
2023-07-04 10:44:22 +0900

    Initial commit

---
+++ src/main/java/com/development/ssosung/SsosungApplication.java
@@ -0,0 +1,14 @@
+package com.development.ssosung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsosungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsosungApplication.class, args);
+        System.out.println("Developer SsoSung");
+    }
+
+}
---
+++ src/test/java/com/development/ssosung/SsosungApplicationTests.java
@@ -0,0 +1,13 @@
+package com.development.ssosung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsosungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
