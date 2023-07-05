e186dcb8a8ab6a1b3e998982ebe11dc26afa4339
Hyun Sung Kim <hskim@nexa-ds.com>
2023-07-05 12:59:44 +0900

    MethodArgumentNotValidException 핸들러추가

--- src/main/java/com/development/ssosung/global/exception/RestControllerExceptionHandler.java
+++ src/main/java/com/development/ssosung/global/exception/RestControllerExceptionHandler.java
@@ -4,11 +4,14 @@ import com.development.ssosung.global.common.SsoSungApiResponse;
 import com.development.ssosung.global.common.SsoSungStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class RestControllerExceptionHandler {
@@ -24,5 +27,17 @@ public class RestControllerExceptionHandler {
         );
 
         return new ResponseEntity<>(ssoSungApiException, HttpStatus.BAD_REQUEST);
+    }
+
+    // @Valid 체크
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleApiRequestException(MethodArgumentNotValidException e) {
+        SsoSungApiResponse ssoSungApiException = new SsoSungApiResponse(
+                SsoSungStatus.SYSTEM_ERROR,
+                e.getMessage(),
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+
+        return new ResponseEntity<>(ssoSungApiException, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
