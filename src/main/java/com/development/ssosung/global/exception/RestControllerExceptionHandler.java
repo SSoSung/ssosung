bae172bc60eace66625c3c2fc0886f04054b4cd7
Hyun Sung Kim <hskim@nexa-ds.com>
2023-07-05 11:04:10 +0900

    BadRequestException 추가

---
+++ src/main/java/com/development/ssosung/global/exception/RestControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.development.ssosung.global.exception;
+
+import com.development.ssosung.global.common.SsoSungApiResponse;
+import com.development.ssosung.global.common.SsoSungStatus;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
+@RestControllerAdvice
+public class RestControllerExceptionHandler {
+
+
+    // BadRequestException 발생 시 Response 에 감싸서 던진다.
+    @ExceptionHandler(value = BadRequestException.class)
+    public ResponseEntity<SsoSungApiResponse> handleApiRequestException(BadRequestException e){
+        SsoSungApiResponse ssoSungApiException = new SsoSungApiResponse(
+                SsoSungStatus.BAD_REQUEST,
+                e.getMessage(),
+                ZonedDateTime.now(ZoneId.of("Z"))
+        );
+
+        return new ResponseEntity<>(ssoSungApiException, HttpStatus.BAD_REQUEST);
+    }
+}
