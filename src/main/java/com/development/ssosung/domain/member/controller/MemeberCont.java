2c9700d9fe80d7ada6a5e4925139d31cdf7b3060
Hyun Sung Kim <gustjd1209@naver.com>
2023-07-08 23:55:16 +0900

    추가 test

--- src/main/java/com/development/ssosung/domain/member/controller/MemeberCont.java
+++ src/main/java/com/development/ssosung/domain/member/controller/MemeberCont.java
@@ -1,9 +1,13 @@
 package com.development.ssosung.domain.member.controller;
 
+import com.development.ssosung.domain.member.service.MemeberServ;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class MemeberCont {
+
+    private final MemeberServ memeberServ;
+
 }
